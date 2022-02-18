0155a79ee53898b1859fadbed009788d58c2f756
Eraldo Filho <eraldo.oliveira@becomex.com.br>
2022-02-18 02:49:12 -0300

    adicionado arquivos para deploy heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+
+// Serve only the static files form the dist directory
+
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html');
+  });
+
+// Start the app by listening on the default Heroku port
+app.listen(PORT, () => {
+  console.log('Servidor iniciado na porta' + PORT);
+});
