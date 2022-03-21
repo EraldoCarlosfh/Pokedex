0c2b3c1b02eb21cf7008b8c77abb83c679dda11f
EraldoCarlosfh <79254386+EraldoCarlosfh@users.noreply.github.com>
2022-03-21 05:14:30 -0300

    Alterado Layout

--- js/app.js
+++ js/app.js
@@ -10,57 +10,57 @@ const generateHTML = pokemons => pokemons.reduce((accumulator, {
 }) => {
 
     const ElementeTypes = types.map(typeInfo => typeInfo.type.name);
-    
-//     if (id == 1) {
-//         accumulator += `
-//         <div class="cartoes-pokemon">
-//     <div class="cartao-pokemon aberto ${ElementeTypes[0]}" id="cartao-${name}">
-//     <div class="cartao-topo">
-//     <div class="detalhes">
-//         <h2 class="nome">${name}</h2>
-//         <span>#${id}</span>
-//     </div>
-//         <span class="tipo">${ElementeTypes.join(' | ')}</span>
 
-//     <div class="cartao-imagem">
-//         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="Imagem do ${name}">
-//     </div>
-// </div>
-//                 <div class="cartao-informacoes">
-//                     <div class="status">
-//                         <h3>Status</h3>
-//                         <ul>
-//                             <li>HP: 300</li>
-//                             <li>Ataque: 600</li>
-//                             <li>Defesa: 500</li>
-//                             <li>Velocidade: 300</li>
-//                             <li>Total: 1.700</li>
-//                         </ul>
-//                     </div>
+    //     if (id == 1) {
+    //         accumulator += `
+    //         <div class="cartoes-pokemon">
+    //     <div class="cartao-pokemon aberto ${ElementeTypes[0]}" id="cartao-${name}">
+    //     <div class="cartao-topo">
+    //     <div class="detalhes">
+    //         <h2 class="nome">${name}</h2>
+    //         <span>#${id}</span>
+    //     </div>
+    //         <span class="tipo">${ElementeTypes.join(' | ')}</span>
 
-//                     <div class="habilidades">
-//                         <h3>Habilidades</h3>
-//                         <ul>
-//                             <li>Choque do trovão</li>
-//                             <li>Cabeçada</li>
-//                         </ul>
+    //     <div class="cartao-imagem">
+    //         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="Imagem do ${name}">
+    //     </div>
+    // </div>
+    //                 <div class="cartao-informacoes">
+    //                     <div class="status">
+    //                         <h3>Status</h3>
+    //                         <ul>
+    //                             <li>HP: 300</li>
+    //                             <li>Ataque: 600</li>
+    //                             <li>Defesa: 500</li>
+    //                             <li>Velocidade: 300</li>
+    //                             <li>Total: 1.700</li>
+    //                         </ul>
+    //                     </div>
 
-//                     </div>
-//                 </div>
-//             </div>   
-//             </div>
+    //                     <div class="habilidades">
+    //                         <h3>Habilidades</h3>
+    //                         <ul>
+    //                             <li>Choque do trovão</li>
+    //                             <li>Cabeçada</li>
+    //                         </ul>
 
-//             <nav class="listagem">
+    //                     </div>
+    //                 </div>
+    //             </div>   
+    //             </div>
 
-//                 <ul>
-//                     <li class="pokemon ativo" id="${id}">
-//                       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="Cabeça do ${name}">
-//                       <span>${name}</span>
-//                     </li>
-//                 </ul>
-//             </nav>
-//   `;
-//     }
+    //             <nav class="listagem">
+
+    //                 <ul>
+    //                     <li class="pokemon ativo" id="${id}">
+    //                       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg" alt="Cabeça do ${name}">
+    //                       <span>${name}</span>
+    //                     </li>
+    //                 </ul>
+    //             </nav>
+    //   `;
+    //     }
 
     accumulator += `
     <div class="cartoes-pokemon">
